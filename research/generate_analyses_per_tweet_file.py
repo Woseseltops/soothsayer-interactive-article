9e80eb67743a6edfa72982f71aa35ff88a97d84d
Wessel Stoop <wstoop@dvg.local>
2018-06-01 14:58:06 +0200

    Transform tweets to instances with sliding window

--- research/generate_analyses_per_tweet_file.py
+++ research/generate_analyses_per_tweet_file.py
@@ -4,16 +4,55 @@ from json import dumps
 TWEET_FOLDER = 'tweets/'
 OUTPUT_FOLDER = 'analyses/'
 
-analyzers = ['a','b']
+class Instance():
 
-for tweetfile in listdir(TWEET_FOLDER):
-    twitter_user = tweetfile.replace('.txt','')
+    def __init__(self,features,label):
+        self.label = label
+        self.features = features
 
-    for analyzer in analyzers:
-        print('working on analyzer ',analyzer,' for ',twitter_user)
+    def __repr__(self):
+        return str(self.features) + '->' + self.label
 
-        result = open(OUTPUT_FOLDER+twitter_user+'.'+analyzer+'.txt','w')
+def tweet_to_instances(tweet,nr_of_features):
 
-        for tweet_index, tweet in enumerate(open(TWEET_FOLDER+tweetfile)):
-            for character in tweet:
-                result.write(str(tweet_index)+'\t'+dumps({'best_pred_word':'ik','best_pred_char':'i'})+'\n')
+    NOTHING_INDICATOR = '_ '
+
+    instances = []
+
+    enclosed_text = nr_of_features*NOTHING_INDICATOR+tweet+nr_of_features*NOTHING_INDICATOR
+    words = enclosed_text.split()
+    word_index = 0
+
+    while True:
+        label = words[word_index+nr_of_features]
+
+        if label == '_':
+            break
+        else:
+            instances.append(Instance(words[word_index:word_index+nr_of_features],label))
+            word_index += 1
+
+    return instances
+
+if __name__ == '__main__':
+
+    NR_OF_FEATURES = 3
+
+    #Train
+    analyzers = ['a','b']
+
+    #Test
+    for tweetfile in listdir(TWEET_FOLDER):
+        twitter_user = tweetfile.replace('.txt','')
+
+        for analyzer in analyzers:
+            print('working on analyzer ',analyzer,' for ',twitter_user)
+
+            result = open(OUTPUT_FOLDER+twitter_user+'.'+analyzer+'.txt','w')
+
+            for tweet_index, tweet in enumerate(open(TWEET_FOLDER+tweetfile)):
+
+                instances = tweet_to_instances(tweet,NR_OF_FEATURES)
+
+                for character in tweet:
+                    result.write(str(tweet_index)+'\t'+dumps({'best_pred_word':'ik','best_pred_char':'i'})+'\n')
